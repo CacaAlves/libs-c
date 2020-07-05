83399ca65c2659d8b75322ecdfa3b65ebf659f62
CacaAlves <cacazinho99@outlook.com>
2020-07-05 03:33:08 -0300

    Implementing lists

--- lists/list_char/list_char.c
+++ lists/list_char/list_char.c
@@ -26,6 +26,153 @@ void push_list_char(ListChar **list, char value)
     }
     else
     {
-        
+        ListChar *last = (*list);
+        node->next = last->next;
+        last->next = node;
+        (*list) = node;
     }
+}
+
+void unshift_list_char(ListChar **list, char value)
+{
+    if (list == NULL)
+        return;
+
+    if ((*list) == NULL)
+    {
+        push_list_char(list, value);
+    }
+    else
+    {
+        ListChar *node = new_node_list_char(value);
+        ListChar *first = (*list)->next;
+        ListChar *last = (*list);
+        last->next = node;
+        node->next = first;
+    }
+}
+
+void pop_list_char(ListChar **list)
+{
+    if (list == NULL || (*list) == NULL)
+        return;
+
+    if ((*list)->next == (*list))
+    {
+        free(*list);
+        (*list) = NULL;
+    }
+    else
+    {
+        ListChar *aux = (*list)->next;
+        while (aux->next != (*list))
+        {
+            aux = aux->next;
+        }
+
+        aux->next = (*list)->next;
+        free(*list);
+        (*list) = aux;
+    }
+}
+
+void shift_list_char(ListChar **list)
+{
+    if (list == NULL || (*list) == NULL)
+        return;
+
+    if ((*list)->next == (*list))
+    {
+        free(*list);
+        (*list) = NULL;
+    }
+    else
+    {
+        ListChar *first = (*list)->next;
+        (*list)->next = first->next;
+        free(first);
+        first = NULL;
+    }
+}
+
+char peek_list_char(ListChar *list)
+{
+    if (list != NULL)
+    {
+        return ((list->next)->value);
+    }
+}
+
+char top_list_char(ListChar *list)
+{
+    if (list != NULL)
+    {
+        return (list->value);
+    }
+}
+
+long long int index_of_list_char(ListChar *list, char value)
+{
+}
+
+ListChar *includes_list_char(ListChar *list, long long int index)
+{
+}
+
+void delete_list_char(ListChar **list, long long int index)
+{
+}
+
+void free_list_char(ListChar **list)
+{
+    if (list == NULL)
+        return;
+
+    ListChar *aux = (*list)->next;
+    while (aux != (*list))
+    {
+        ListChar *prev = aux;
+        aux = aux->next;
+        free(prev);
+    }
+    free(aux);
+    (*list) = NULL;
+}
+
+long long unsigned int length_list_char(ListChar *list)
+{
+    if (list == NULL)
+        return 0;
+
+    long long unsigned int counter = 0;
+    ListChar *aux = list->next;
+
+    while (aux != list)
+    {
+        counter++;
+        aux = aux->next;
+    }
+    counter++;
+
+    return counter;
+}
+
+bool is_empty_list_char(ListChar *list)
+{
+}
+
+void print_list_char(ListChar *list)
+{
+    if (list == NULL)
+        return;
+
+    ListChar *first = list->next;
+
+    while (first != list)
+    {
+        printf("%c", first->value);
+        first = first->next;
+    }
+
+    printf("%c\n", list->value);
 }
--- lists/list_char/list_char.h
+++ lists/list_char/list_char.h
@@ -6,11 +6,16 @@ typedef struct ListChar
     struct ListChar *next;
 } ListChar;
 
-void push_list_char(ListChar **list, char value);    /*Inserts at the end*/
-void unshift_list_char(ListChar **list, char value); /*Inserts at the beginning*/
-void pop_queue_char(ListChar **list);                /*Removes at the end*/
-void shift_list_char(ListChar **list);               /*Removes at the beginning*/
-char peek_list_char(ListChar *list);                 /*Returns the first value*/
-char top_list_char(ListChar *list);                  /*Returns the the last value*/
-bool is_empty_list_char(ListChar *list);             /*Returns whether the list is empty or not*/
-void print_queue_char(ListChar **list);              /*Prints the list*/
+void push_list_char(ListChar **list, char value);                  /*Inserts at the end*/
+void unshift_list_char(ListChar **list, char value);               /*Inserts at the beginning*/
+void pop_list_char(ListChar **list);                               /*Removes at the end*/
+void shift_list_char(ListChar **list);                             /*Removes at the beginning*/
+char peek_list_char(ListChar *list);                               /*Returns the first value*/
+char top_list_char(ListChar *list);                                /*Returns the the last value*/
+long long int index_of_list_char(ListChar *list, char value);      /*Returns the index of the node that contains the value*/
+ListChar *includes_list_char(ListChar *list, long long int index); /*Returns the node of the index in the list*/
+void delete_list_char(ListChar **list, long long int index);       /*Removes at the index position*/
+void free_list_char(ListChar **list);                              /*Free the list from the memory*/
+long long unsigned int length_list_char(ListChar *list);                    /*Returns the length of the list*/
+bool is_empty_list_char(ListChar *list);                           /*Returns whether the list is empty or not*/
+void print_list_char(ListChar *list);                              /*Prints the list*/
